24a68880cb1b69dcc105c5d040a79bffbc944c53
Kenneth Endfinger <kaendfinger@gmail.com>
2013-10-26 23:26:14 -0400

    Jobs should run in new threads.

--- src/main/groovy/com/directmyfile/ci/CI.groovy
+++ src/main/groovy/com/directmyfile/ci/CI.groovy
@@ -20,7 +20,7 @@ class CI {
     }
 
     private void init() {
-       new File(configRoot, 'logs').mkdirs()
+        new File(configRoot, 'logs').mkdirs()
     }
 
     private def loadJobs() {
@@ -35,44 +35,46 @@ class CI {
     }
 
     void runJob(Job job) {
-        def success = true
-        job.status = JobStatus.RUNNING
-        println "Running Job: ${job.name}"
+        Thread.start("Builder[${job.name}]") {
+            def success = true
+            job.status = JobStatus.RUNNING
+            println "Running Job: ${job.name}"
 
-        job.getSCM().clone(job.buildDir)
+            job.getSCM().clone(job.buildDir)
 
-        def tasks = job.tasks
+            def tasks = job.tasks
 
-        for (task in tasks) {
-            def id = tasks.indexOf(task) + 1
-            println "Running Task ${id} of ${job.tasks.size()}"
-            def taskSuccess = task.task.execute(task.params)
+            for (task in tasks) {
+                def id = tasks.indexOf(task) + 1
+                println "Running Task ${id} of ${job.tasks.size()}"
+                def taskSuccess = task.task.execute(task.params)
 
-            if (!taskSuccess) {
-                println 'Task has Failed'
-                success = false
-                break
+                if (!taskSuccess) {
+                    println 'Task has Failed'
+                    success = false
+                    break
+                }
+
+                println 'Task has Completed'
+            }
+            def artifacts = new File(artifactDir, "${job.name}")
+            artifacts.mkdir()
+            job.artifactLocations.each {
+                def file = new File(job.buildDir, it)
+                if (!file.exists()) {
+                    job.logFile.append("\nArtifact File: ${file.canonicalPath} does not exist")
+                    return
+                }
+                new File(artifacts, file.name).bytes = file.bytes
             }
 
-            println 'Task has Completed'
-        }
-        def artifacts = new File(artifactDir, "${job.name}")
-        artifacts.mkdir()
-        job.artifactLocations.each {
-            def file = new File(job.buildDir, it)
-            if (!file.exists()) {
-                job.logFile.append("\nArtifact File: ${file.canonicalPath} does not exist")
-                return
+            if (!success) {
+                println 'Job has Failed'
+                job.status = JobStatus.FAILURE
+            } else {
+                println 'Job has Completed'
+                job.status = JobStatus.SUCCESS
             }
-            new File(artifacts, file.name).bytes = file.bytes
-        }
-
-        if (!success) {
-            println 'Job has Failed'
-            job.status = JobStatus.FAILURE
-        } else {
-            println 'Job has Completed'
-            job.status = JobStatus.SUCCESS
         }
     }
 
