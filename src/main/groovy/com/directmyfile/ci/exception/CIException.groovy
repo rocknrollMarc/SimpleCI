bee9f006bb6789ec9cca0f3ed042bc0117b73c4c
CodeBot <codebot@directmyfile.com>
2013-12-04 13:09:47 -0500

    Make gh-pages branch

--- src/main/groovy/com/directmyfile/ci/exception/CIException.groovy
+++
@@ -1,7 +0,0 @@
-package com.directmyfile.ci.exception
-
-import groovy.transform.InheritConstructors
-
-@InheritConstructors
-class CIException extends Exception {
-}
--- src/main/groovy/com/directmyfile/ci/exception/JobConfigurationException.groovy
+++
@@ -1,7 +0,0 @@
-package com.directmyfile.ci.exception
-
-import groovy.transform.InheritConstructors
-
-@InheritConstructors
-class JobConfigurationException extends CIException {
-}
