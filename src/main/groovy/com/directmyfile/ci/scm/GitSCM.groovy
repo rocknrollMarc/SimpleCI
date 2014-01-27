219d8533668467d339ff1d67ad89615bb61a98a5
Logan Gorence <loganjohngorence@gmail.com>
2014-01-26 19:24:00 -0800

    Fix a file not found exception due to attempting to retrieve the changes via the json api.

--- src/main/groovy/com/directmyfile/ci/scm/GitSCM.groovy
+++ src/main/groovy/com/directmyfile/ci/scm/GitSCM.groovy
@@ -18,10 +18,10 @@ class GitSCM extends SCM {
 
     @Override
     void clone(Job job) {
-
         def cmd = [findGit().absolutePath, "clone", "--recursive", job.getSCM().getUrl(), job.buildDir.absolutePath]
 
         def proc = execute(job, cmd)
+        job.logFile.mkdirs()
         def log = job.logFile.newPrintWriter()
         proc.inputStream.eachLine {
             log.println(it)
